0c614ed54106b81a07d24bed7009f3bef91ce929
trinhmanhcuong2001 <cuong69pro@gmail.com>
2024-10-17 11:58:28 +0700

    upload

--- database/migrations/2024_10_17_034947_create_posts_table.php
+++ database/migrations/2024_10_17_034947_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->date('published_at')->nullable();
+            $table->datetime('published_at')->nullable();
         });
     }
 
